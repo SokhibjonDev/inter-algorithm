18cf112c7ce2f07ed1f403ede4cfd7e2e549c5da
SokhibjonDev <110424000+SokhibjonDev@users.noreply.github.com>
2022-08-30 09:09:18 +0500

    feat : two sum [ array ]

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // ## // ## array reverse algotirm
 
-const arr = [1, 2, 3, 4, 5, 6]
+// const arr = [1, 2, 3, 4, 5, 6]
 
-for (let i = arr.length - 2; i >= 0; i--) {
-    arr.push(arr.splice(i, 1)[0])
-}
-console.log(arr);
+// for (let i = arr.length - 2; i >= 0; i--) {
+//     arr.push(arr.splice(i, 1)[0])
+// }
+// console.log(arr);
+
+
+// ## // ## two sum
+
+var twoSum = function (nums, target) {
+    for (let i = 0; i <= nums.lenght - 1; i++) {
+        let item1 = nums[i]
+        for (let j = i + 1; j <= nums.lenght - 1; j++) {
+            let item2 = nums[j]
+            let sum = item1 + item2
+            if (sum === target) {
+                return [i, j]
+            }
+        }
+    };
+};
